4a46073a51d742048fc322ae8859f4a28f32405d
jonathanfishbein1 <jonathanfishbein@protonmail.com>
2021-11-15 21:15:22 +0000

    Add validator

--- swap-on-chain/Script.hs
+++ swap-on-chain/Script.hs
@@ -12,10 +12,14 @@
 
 module Script where
 
+import qualified Data.ByteString
+import qualified Flat
 import qualified Plutus.V1.Ledger.Ada
 import qualified Plutus.V1.Ledger.Address
+import qualified Plutus.V1.Ledger.Address as Plutus.V1.Ledger
 import qualified Plutus.V1.Ledger.Contexts
-import Plutus.V1.Ledger.Scripts
+import qualified Plutus.V1.Ledger.Scripts
+import Plutus.V2.Ledger.Api (ScriptContext (ScriptContext))
 import qualified PlutusTx
 import qualified PlutusTx.Prelude
 
@@ -26,12 +30,15 @@ data TokenSaleParams = TokenSaleParams
 
 PlutusTx.makeLift ''TokenSaleParams
 
-{-# INLINEABLE mkValidator #-}
-mkValidator :: TokenSaleParams -> PlutusTx.BuiltinData -> PlutusTx.BuiltinData -> Plutus.V1.Ledger.Contexts.ScriptContext -> ()
-mkValidator ts _ _ context = PlutusTx.Prelude.traceError "BURNT!"
+{-# INLINEABLE mkTokenSaleValidator #-}
+mkTokenSaleValidator :: TokenSaleParams -> PlutusTx.BuiltinData -> PlutusTx.BuiltinData -> PlutusTx.BuiltinData -> ()
+mkTokenSaleValidator ts _ _ context = PlutusTx.Prelude.traceError "BURNT!"
   where
+    valCtx :: Plutus.V1.Ledger.Contexts.ScriptContext
+    valCtx = PlutusTx.unsafeFromBuiltinData context
+
     info :: Plutus.V1.Ledger.Contexts.TxInfo
-    info = Plutus.V1.Ledger.Contexts.scriptContextTxInfo context
+    info = Plutus.V1.Ledger.Contexts.scriptContextTxInfo valCtx
 
     currentTxOutputs :: [Plutus.V1.Ledger.Contexts.TxOut]
     currentTxOutputs = Plutus.V1.Ledger.Contexts.txInfoOutputs info
@@ -44,12 +51,25 @@ mkValidator ts _ _ context = PlutusTx.Prelude.traceError "BURNT!"
     sellerAddress = tsSellerAddress ts
 
     contextCostCheck :: [Plutus.V1.Ledger.Contexts.TxOut] -> PlutusTx.Prelude.Bool
-    contextCostCheck [] = PlutusTx.Prelude.traceIfFalse "Incorrect Amount Of ADA Sent To Script Address" PlutusTx.Prelude.$ PlutusTx.Prelude.False
+    contextCostCheck [] = PlutusTx.Prelude.traceIfFalse "Incorrect Amount Of ADA Sent To Script Address" PlutusTx.Prelude.False
     contextCostCheck (x : xs)
-      | ((Plutus.V1.Ledger.Contexts.txOutAddress x) PlutusTx.Prelude.== sellerAddress) PlutusTx.Prelude.&& ((Plutus.V1.Ledger.Contexts.txOutValue x) PlutusTx.Prelude.== (Plutus.V1.Ledger.Ada.lovelaceValueOf tokenCost)) = PlutusTx.Prelude.True
+      | (Plutus.V1.Ledger.Contexts.txOutAddress x PlutusTx.Prelude.== sellerAddress) PlutusTx.Prelude.&& (Plutus.V1.Ledger.Contexts.txOutValue x PlutusTx.Prelude.== Plutus.V1.Ledger.Ada.lovelaceValueOf tokenCost) = PlutusTx.Prelude.True
       | PlutusTx.Prelude.otherwise = contextCostCheck xs
 
--- data Typed
--- instance Plutus.V1.Ledger.Scripts.Script Typed where
---     type instance DatumType    Typed = PlutusTx.BuiltinData
---     type instance RedeemerType Typed = PlutusTx.BuiltinData
+tokenSaleCompiled :: TokenSaleParams -> PlutusTx.CompiledCode (PlutusTx.BuiltinData -> PlutusTx.BuiltinData -> PlutusTx.BuiltinData -> ())
+tokenSaleCompiled ts = $$(PlutusTx.compile [||mkTokenSaleValidator||]) `PlutusTx.applyCode` PlutusTx.liftCode ts
+
+validator :: Plutus.V1.Ledger.Scripts.Validator
+validator = Plutus.V1.Ledger.Scripts.mkValidatorScript (tokenSaleCompiled ts)
+  where
+    ts =
+      TokenSaleParams
+        { tsSellerAddress = Plutus.V1.Ledger.pubKeyHashAddress "PUBLIC_KEY_HASH_ADDRESS",
+          tsCost = 10
+        }
+
+serializedCompiledCode :: TokenSaleParams -> Data.ByteString.ByteString
+serializedCompiledCode ts = Flat.flat (tokenSaleCompiled ts)
+
+-- validatorCodeFromFile :: PlutusTx.CompiledCode (() -> () -> Plutus.V1.Ledger.Contexts.ScriptContext -> PlutusTx.Prelude.Bool)
+-- validatorCodeFromFile = $$(PlutusTx.loadFromFile "plutus/howtos/myscript.uplc")
