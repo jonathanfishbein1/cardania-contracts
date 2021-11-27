f6cd75e01e99d01c65b26aa0ce7ecec913df1c38
jonathanfishbein1 <jonathanfishbein@protonmail.com>
2021-11-27 16:17:57 +0000

    Add Plutus script

--- swap-on-chain/Main.hs
+++ swap-on-chain/Main.hs
@@ -1,4 +1,46 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE NoImplicitPrelude     #-}
+
 module Main where
 
-main :: IO ()
-main = putStrLn "Hello, Haskell!"
+import qualified Cardano.Api
+import qualified Data.ByteString.Short
+import qualified Data.String
+import qualified Plutus.V1.Ledger.Api
+import qualified PlutusTx.Builtins.Class
+import qualified Script
+import qualified System.Environment
+import qualified Prelude
+main :: Prelude.IO ()
+main = 
+    do
+    case Plutus.V1.Ledger.Api.defaultCostModelParams of
+      Prelude.Just m ->
+        let (logout, e) =
+              Plutus.V1.Ledger.Api.evaluateScriptCounting
+                Plutus.V1.Ledger.Api.Verbose
+                m
+                Script.tokenSaleSBS
+                [ Plutus.V1.Ledger.Api.toData  (PlutusTx.Builtins.Class.stringToBuiltinByteString "Hello World!"),
+                  Plutus.V1.Ledger.Api.toData (),
+                  Plutus.V1.Ledger.Api.toData ()
+                ]
+         in do
+              Prelude.print ("e" Prelude.++ (Prelude.show e))
+              Prelude.print ("Log output" :: Prelude.String) Prelude.>> Prelude.print logout
+              Prelude.print ("e" Prelude.++ (Prelude.show e))
+              case e of
+                Prelude.Left evalErr -> Prelude.print ("Eval Error" :: Prelude.String) Prelude.>> Prelude.print evalErr
+                Prelude.Right exbudget -> Prelude.print ("Ex Budget" :: Prelude.String) Prelude.>> Prelude.print exbudget
+      Prelude.Nothing -> Prelude.error "defaultCostModelParams failed"
+    result <- Cardano.Api.writeFileTextEnvelope "result.plutus" Prelude.Nothing Script.tokenSaledSerialised
+    case result of
+      Prelude.Left err -> Prelude.print Prelude.$ Cardano.Api.displayError err
+      Prelude.Right () -> Prelude.return ()
--- swap-on-chain/Script.hs
+++ swap-on-chain/Script.hs
@@ -1,4 +1,7 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingVia #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings #-}
@@ -7,21 +10,33 @@
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE NoImplicitPrelude #-}
-{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+{-# LANGUAGE NoImplicitPrelude     #-}
 
-module Script where
+module Script
+  ( 
+    tokenSaledSerialised,
+    tokenSaleSBS,
+  )
+where
 
+import qualified Cardano.Api
+import qualified Cardano.Api.Shelley
+import qualified Codec.Serialise
+import qualified Data.Aeson
 import qualified Data.ByteString
-import qualified Flat
+import qualified Data.ByteString.Lazy
+import qualified Data.ByteString.Short
 import qualified Plutus.V1.Ledger.Ada
+
+
 import qualified Plutus.V1.Ledger.Address
-import qualified Plutus.V1.Ledger.Address as Plutus.V1.Ledger
 import qualified Plutus.V1.Ledger.Contexts
 import qualified Plutus.V1.Ledger.Scripts
-import Plutus.V2.Ledger.Api (ScriptContext (ScriptContext))
+import qualified Plutus.V1.Ledger.Api
 import qualified PlutusTx
 import qualified PlutusTx.Prelude
+import qualified Ledger.Typed.Scripts 
+import qualified Prelude
 
 data TokenSaleParams = TokenSaleParams
   { tsSellerAddress :: !Plutus.V1.Ledger.Address.Address,
@@ -30,46 +45,61 @@ data TokenSaleParams = TokenSaleParams
 
 PlutusTx.makeLift ''TokenSaleParams
 
+data TokenSale
+
+instance Ledger.Typed.Scripts.ValidatorTypes TokenSale where
+    type instance RedeemerType TokenSale = ()
+    type instance DatumType TokenSale = ()
+
+
 {-# INLINEABLE mkTokenSaleValidator #-}
-mkTokenSaleValidator :: TokenSaleParams -> PlutusTx.BuiltinData -> PlutusTx.BuiltinData -> PlutusTx.BuiltinData -> ()
-mkTokenSaleValidator ts _ _ context = PlutusTx.Prelude.traceError "BURNT!"
-  where
-    valCtx :: Plutus.V1.Ledger.Contexts.ScriptContext
-    valCtx = PlutusTx.unsafeFromBuiltinData context
+mkTokenSaleValidator :: TokenSaleParams -> () -> () -> Plutus.V1.Ledger.Contexts.ScriptContext -> PlutusTx.Prelude.Bool
+mkTokenSaleValidator ts () () context = PlutusTx.Prelude.True
+  -- where
 
-    info :: Plutus.V1.Ledger.Contexts.TxInfo
-    info = Plutus.V1.Ledger.Contexts.scriptContextTxInfo valCtx
+  --   info :: Plutus.V1.Ledger.Contexts.TxInfo
+  --   info = Plutus.V1.Ledger.Contexts.scriptContextTxInfo context
 
-    currentTxOutputs :: [Plutus.V1.Ledger.Contexts.TxOut]
-    currentTxOutputs = Plutus.V1.Ledger.Contexts.txInfoOutputs info
+  --   currentTxOutputs :: [Plutus.V1.Ledger.Contexts.TxOut]
+  --   currentTxOutputs = Plutus.V1.Ledger.Contexts.txInfoOutputs info
 
-    tokenCost :: PlutusTx.Prelude.Integer
-    tokenCost = tsCost ts
+  --   tokenCost :: PlutusTx.Prelude.Integer
+  --   tokenCost = tsCost ts
 
-    sellerAddress :: Plutus.V1.Ledger.Address.Address
+  --   sellerAddress :: Plutus.V1.Ledger.Address.Address
+  --   sellerAddress = tsSellerAddress ts
 
-    sellerAddress = tsSellerAddress ts
+  --   contextCostCheck :: [Plutus.V1.Ledger.Contexts.TxOut] -> PlutusTx.Prelude.Bool
+  --   contextCostCheck [] = PlutusTx.Prelude.traceIfFalse "Incorrect Amount Of ADA Sent To Script Address" PlutusTx.Prelude.False
+  --   contextCostCheck (x : xs)
+  --     | (Plutus.V1.Ledger.Contexts.txOutAddress x PlutusTx.Prelude.== sellerAddress) PlutusTx.Prelude.&& (Plutus.V1.Ledger.Contexts.txOutValue x PlutusTx.Prelude.== Plutus.V1.Ledger.Ada.lovelaceValueOf tokenCost) = PlutusTx.Prelude.True
+  --     | PlutusTx.Prelude.otherwise = contextCostCheck xs
 
-    contextCostCheck :: [Plutus.V1.Ledger.Contexts.TxOut] -> PlutusTx.Prelude.Bool
-    contextCostCheck [] = PlutusTx.Prelude.traceIfFalse "Incorrect Amount Of ADA Sent To Script Address" PlutusTx.Prelude.False
-    contextCostCheck (x : xs)
-      | (Plutus.V1.Ledger.Contexts.txOutAddress x PlutusTx.Prelude.== sellerAddress) PlutusTx.Prelude.&& (Plutus.V1.Ledger.Contexts.txOutValue x PlutusTx.Prelude.== Plutus.V1.Ledger.Ada.lovelaceValueOf tokenCost) = PlutusTx.Prelude.True
-      | PlutusTx.Prelude.otherwise = contextCostCheck xs
 
-tokenSaleCompiled :: TokenSaleParams -> PlutusTx.CompiledCode (PlutusTx.BuiltinData -> PlutusTx.BuiltinData -> PlutusTx.BuiltinData -> ())
-tokenSaleCompiled ts = $$(PlutusTx.compile [||mkTokenSaleValidator||]) `PlutusTx.applyCode` PlutusTx.liftCode ts
 
-validator :: Plutus.V1.Ledger.Scripts.Validator
-validator = Plutus.V1.Ledger.Scripts.mkValidatorScript (tokenSaleCompiled ts)
+typedValidator :: TokenSaleParams -> Ledger.Typed.Scripts.TypedValidator TokenSale
+typedValidator ts = Ledger.Typed.Scripts.mkTypedValidator @TokenSale
+    ($$(PlutusTx.compile [||mkTokenSaleValidator||])  `PlutusTx.applyCode` PlutusTx.liftCode ts)
+    $$(PlutusTx.compile [|| Ledger.Typed.Scripts.wrapValidator @() @() ||])
+    where
+        wrap = Ledger.Typed.Scripts.wrapValidator @() @()
+
+
+validator :: TokenSaleParams ->  Plutus.V1.Ledger.Scripts.Validator
+validator = Ledger.Typed.Scripts.validatorScript PlutusTx.Prelude.. typedValidator
+
+
+tokenSaleScript :: Plutus.V1.Ledger.Scripts.Script
+tokenSaleScript = Plutus.V1.Ledger.Scripts.unValidatorScript (validator ts)
   where
     ts =
       TokenSaleParams
-        { tsSellerAddress = Plutus.V1.Ledger.pubKeyHashAddress "PUBLIC_KEY_HASH_ADDRESS",
-          tsCost = 10
+        { tsSellerAddress = Plutus.V1.Ledger.Address.pubKeyHashAddress "eefb5b9dbac4a380296de0655f6ace6c97e9b981eef89a7bf53dcd52",
+          tsCost = 10000000
         }
 
-serializedCompiledCode :: TokenSaleParams -> Data.ByteString.ByteString
-serializedCompiledCode ts = Flat.flat (tokenSaleCompiled ts)
+tokenSaleSBS :: Data.ByteString.Short.ShortByteString
+tokenSaleSBS = Data.ByteString.Short.toShort PlutusTx.Prelude.. Data.ByteString.Lazy.toStrict PlutusTx.Prelude.$ Codec.Serialise.serialise tokenSaleScript
 
--- validatorCodeFromFile :: PlutusTx.CompiledCode (() -> () -> Plutus.V1.Ledger.Contexts.ScriptContext -> PlutusTx.Prelude.Bool)
--- validatorCodeFromFile = $$(PlutusTx.loadFromFile "plutus/howtos/myscript.uplc")
+tokenSaledSerialised :: Cardano.Api.PlutusScript Cardano.Api.PlutusScriptV1
+tokenSaledSerialised = Cardano.Api.Shelley.PlutusScriptSerialised tokenSaleSBS
