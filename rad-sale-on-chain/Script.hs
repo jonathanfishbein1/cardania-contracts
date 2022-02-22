4d7f40d3aebe92889204a74272018c2f33fc96ab
jonathanfishbein1 <jonathanfishbein@protonmail.com>
2022-02-22 19:30:51 +0000

    Add logic for seller address

--- rad-sale-on-chain/Script.hs
+++ rad-sale-on-chain/Script.hs
@@ -32,6 +32,7 @@ import qualified Plutus.V1.Ledger.Ada
 import qualified Plutus.V1.Ledger.Address
 import qualified Plutus.V1.Ledger.Api
 import qualified Plutus.V1.Ledger.Contexts
+import qualified Plutus.V1.Ledger.Crypto
 import qualified Plutus.V1.Ledger.Scripts
 import qualified PlutusTx
 import qualified PlutusTx.Lift.Class
@@ -53,19 +54,19 @@ mkRadSaleOnChainValidator _ _ context
     info :: Plutus.V1.Ledger.Contexts.TxInfo
     info = Plutus.V1.Ledger.Contexts.scriptContextTxInfo context
 
+    currentTxOutputs :: [Plutus.V1.Ledger.Contexts.TxOut]
     currentTxOutputs = Plutus.V1.Ledger.Contexts.txInfoOutputs info
 
     tokenCost :: PlutusTx.Prelude.Integer
     tokenCost = 10000000
 
-    -- sellerAddress :: Plutus.V1.Ledger.Api.Address
-    -- sellerAddress = Plutus.V1.Ledger.Address.pubKeyHashAddress ""
+    sellerAddress :: Plutus.V1.Ledger.Api.Address
+    sellerAddress = Plutus.V1.Ledger.Address.pubKeyHashAddress (Plutus.V1.Ledger.Crypto.PubKeyHash "eefb5b9dbac4a380296de0655f6ace6c97e9b981eef89a7bf53dcd52")
 
     contextCostCheck [] = PlutusTx.Prelude.traceIfFalse "Incorrect Amount Of ADA Sent To Script Address" PlutusTx.Prelude.False
     contextCostCheck (x : xs)
-      | --((Plutus.V1.Ledger.Contexts.txOutAddress x) PlutusTx.Prelude.== sellerAddress) PlutusTx.Prelude.&&
-
-        Plutus.V1.Ledger.Contexts.txOutValue x PlutusTx.Prelude.== Ledger.Ada.lovelaceValueOf tokenCost =
+      | (Plutus.V1.Ledger.Contexts.txOutAddress x PlutusTx.Prelude.== sellerAddress)
+          PlutusTx.Prelude.&& Plutus.V1.Ledger.Contexts.txOutValue x PlutusTx.Prelude.== Ledger.Ada.lovelaceValueOf tokenCost =
         PlutusTx.Prelude.True
       | PlutusTx.Prelude.otherwise = contextCostCheck xs
 
