c8e39256d50d1aee6f2e0b8e501ad6b3814e187c
jonathanfishbein1 <jonathanfishbein@protonmail.com>
2022-03-19 22:26:21 +0000

    Put 100 ada into wallet 2

--- rad-sale-on-chain/Emulator.hs
+++ rad-sale-on-chain/Emulator.hs
@@ -55,6 +55,17 @@ import qualified Text.Printf
 import qualified Wallet.Emulator.Wallet
 import qualified Prelude
 
+tokenSaleParam =
+  Script.TokenSaleParam
+    { Script.tokenCost = 10000000,
+      Script.currencySymbol = "641593ca39c5cbd3eb314533841d53e61ebf6ee7a0ec7c391652f31e",
+      Script.tokenName = "CardaniaFounderWhite",
+      Script.sellerPubKeyHash =
+        Ledger.Address.unPaymentPubKeyHash PlutusTx.Prelude.$
+          Wallet.Emulator.Wallet.mockWalletPaymentPubKeyHash PlutusTx.Prelude.$
+            Wallet.Emulator.Wallet.knownWallet 1
+    }
+
 myTrace :: Plutus.Trace.EmulatorTrace ()
 myTrace = do
   h1 <-
@@ -66,29 +77,35 @@ myTrace = do
       (Wallet.Emulator.Wallet.knownWallet 2)
       Script.endpoints
   Plutus.Trace.callEndpoint @"start" h1 PlutusTx.Prelude.$
-    Script.TokenSaleParam
-      { Script.tokenCost = 10000000,
-        Script.currencySymbol = "641593ca39c5cbd3eb314533841d53e61ebf6ee7a0ec7c391652f31e",
-        Script.tokenName = "CardaniaFounderWhite",
-        Script.sellerPubKeyHash =
-          Ledger.Address.unPaymentPubKeyHash PlutusTx.Prelude.$
-            Wallet.Emulator.Wallet.mockWalletPaymentPubKeyHash PlutusTx.Prelude.$
-              Wallet.Emulator.Wallet.knownWallet 1
-      }
+    tokenSaleParam
+  s <- Plutus.Trace.Emulator.waitNSlots 2
+  Plutus.Trace.callEndpoint @"buy" h2 PlutusTx.Prelude.$
+    tokenSaleParam
+  s2 <- Plutus.Trace.Emulator.waitNSlots 2
+  Control.Monad.Freer.Extras.logInfo PlutusTx.Prelude.$
+    "reached " PlutusTx.Prelude.++ Prelude.show 1
 
-v =
+tokenValue :: Plutus.V1.Ledger.Value.Value
+tokenValue =
   Plutus.V1.Ledger.Api.singleton
     "641593ca39c5cbd3eb314533841d53e61ebf6ee7a0ec7c391652f31e"
     "CardaniaFounderWhite"
     1
     PlutusTx.Prelude.<> Ledger.Ada.lovelaceValueOf 10000000
 
+walletValue :: Plutus.V1.Ledger.Value.Value
+walletValue =
+  Ledger.Ada.lovelaceValueOf 100_000_000
+
 emulatorConfig :: Plutus.Trace.Emulator.EmulatorConfig
 emulatorConfig =
   Plutus.Trace.EmulatorConfig
     { Plutus.Trace._initialChainState =
-        PlutusTx.Prelude.Left
-          (Data.Map.singleton (Wallet.Emulator.Wallet.knownWallet 1) v),
+        PlutusTx.Prelude.Left PlutusTx.Prelude.$
+          Data.Map.fromList
+            [ ((Wallet.Emulator.Wallet.knownWallet 1), tokenValue),
+              ((Wallet.Emulator.Wallet.knownWallet 2), walletValue)
+            ],
       Plutus.Trace._slotConfig = Data.Default.def,
       Plutus.Trace._feeConfig = Data.Default.def
     }
