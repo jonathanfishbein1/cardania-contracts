86d19ff364f8eeb67794d7d223fc577dbdcc6d61
jonathanfishbein1 <jonathanfishbein@protonmail.com>
2022-03-28 00:07:54 +0000

    Fix up burn script

--- mint-using-plutus/transactions/burn.sh
+++ mint-using-plutus/transactions/burn.sh
@@ -1,35 +1,41 @@
-cardano-cli query protocol-parameters --mainnet --out-file protocol.json
+utxovkey=/home/jonathan/Documents/octoberFestMetaData/minting.vkey
+utxoskey=/home/jonathan/Documents/octoberFestMetaData/minting.skey
 
-walletAddress="addr1qxldw6tu3l644fftqpepp9rz6c76kwcna5dnf9dh6lp6jjq32t906vdyzfah2x6rt4920uh42fz43extjglwftjym33qm66ygl"
-wallettxOut="$walletAddress+1500000"
+transactionsPath=/home/jonathan/Documents/cardania-contracts/mint-using-plutus/transactions/
+
+cardano-cli query protocol-parameters --testnet-magic 1097911063 --out-file "${transactionsPath}protocol.json"
+
+mintingWalletAddress="addr_test1vrh0kkuahtz28qpfdhsx2hm2eekf06des8h03xnm757u65sd6egwy"
+wallettxOut="$mintingWalletAddress+1500000"
+policyFile="${transactionsPath}result.plutus"
+policyId=$(cardano-cli transaction policyid --script-file $policyFile)
+
+mint="-2 $policyId.426f6e6573"
+txuot="${wallettxOut}" 
 
 
-policyId="d0b4c7811012fc5e9860c2fe374265f4e465ff99586ed7352fa9a866"
-
-
-mint="-1 $policyId.WarpFuelGalactic13"
-txuot="${wallettxOut}+$mint" 
-
-
-scriptownerUtxo1="0ec40b6a02746fe9a19679deceb14f84eb7f0955fcdd70eb74c2e2d25c85853f#0"
+scriptownerUtxo="64d175b8b695261edc83cf5350fb4c6e283108c9121e7928aa3eb0e71c7ad280#0"
+collateralUtxo="fa6452e94b21815aea7e829a666b2f73506ee694925c5d7f92e3c41ad7772734#0"
 
 cardano-cli transaction build \
-  --alonzo-era \
-  --cardano-mode \
-  --mainnet \
-  --tx-in "$scriptownerUtxo1" \
+  --testnet-magic 1097911063 \
+  --tx-in "$scriptownerUtxo" \
+  --tx-in "$collateralUtxo" \
+  --tx-in-collateral "$collateralUtxo" \
   --tx-out "$txuot" \
-  --change-address "$walletAddress" \
+  --change-address "$mintingWalletAddress" \
   --mint="$mint" \
-  --mint-script-file "policy.script" \
-  --metadata-json-file "./burningMetadatas/WarpFuelGalactic13.json" \
-  --protocol-params-file protocol.json \
-  --out-file "plutusburn.body"
+  --mint-script-file "${transactionsPath}result.plutus" \
+  --mint-redeemer-file "${transactionsPath}unit.json" \
+  --required-signer-hash eefb5b9dbac4a380296de0655f6ace6c97e9b981eef89a7bf53dcd52 \
+  --metadata-json-file "/home/jonathan/Documents/cardania-contracts/mint-using-plutus/metadata/AntigravityParticles.json" \
+  --protocol-params-file "${transactionsPath}protocol.json" \
+  --out-file "${transactionsPath}mint.body"
 
 cardano-cli transaction sign \
-  --tx-body-file "plutusburn.body" \
-  --mainnet \
+  --tx-body-file "${transactionsPath}mint.body" \
+  --testnet-magic 1097911063 \
   --signing-key-file "$utxoskey" \
-  --out-file "plutusburn.tx"
+  --out-file "${transactionsPath}mint.tx"
 
-cardano-cli transaction submit --tx-file "plutusmint.tx" --mainnet
+cardano-cli transaction submit --tx-file "${transactionsPath}mint.tx" --testnet-magic 1097911063
