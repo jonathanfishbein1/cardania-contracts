185af9bd06275c304ba64994e1bcca072a86e984
jonathanfishbein1 <jonathanfishbein@protonmail.com>
2022-04-06 21:00:21 +0000

    Import plutus-apps shell

--- shell.nix
+++ shell.nix
@@ -1,11 +1,10 @@
 let
   project = import ./project.nix;
   nixpkgs = import <nixpkgs> { };
+  plutus-apps-shell = import /home/jonathan/Documents/plutus-apps/shell.nix;
 in
 project.shellFor {
-  buildInputs = [
-    nixpkgs.git
-  ];
+  inputsFrom = [ plutus-apps-shell ];
   tools = {
     cabal = "3.6.0.0";
     hlint = "latest"; # Selects the latest version in the hackage.nix snapshot
