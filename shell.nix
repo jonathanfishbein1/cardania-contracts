e3b49e09fb36e1542433588e78979a6185acd8b6
jonathanfishbein1 <jonathanfishbein@protonmail.com>
2022-04-07 01:25:53 +0000

    Just import plutus-apps shell

--- shell.nix
+++ shell.nix
@@ -1,13 +1,4 @@
 let
-  project = import ./project.nix;
-  nixpkgs = import <nixpkgs> { };
-  plutus-apps-shell = import /home/jonathan/Documents/plutus-apps/shell.nix;
+  plutus-apps-shell = import ../plutus-apps/shell.nix { };
 in
-project.shellFor {
-  inputsFrom = [ plutus-apps-shell ];
-  tools = {
-    cabal = "3.6.0.0";
-    hlint = "latest"; # Selects the latest version in the hackage.nix snapshot
-    haskell-language-server = "latest";
-  };
-}
+plutus-apps-shell
