cae2b94daf059c07623c6246707d7942145d2467
jmagan <jmaganvalero@gmail.com>
2022-05-03 14:12:14 +0200

    React interface for plutus-app demo and RAD sale

---
+++ rad-sale-on-chain/pab-nami/client-react/src/App.jsx
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react';
+import TokenSale from './pages/TokenSale';
+
+export function App() {
+    return (
+    <Fragment>
+        <TokenSale></TokenSale>
+    </Fragment>
+    )
+}
