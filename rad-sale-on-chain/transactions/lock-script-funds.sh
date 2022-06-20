ab2ad27cfdca304d5daa0a489c093ffd551d2ae8
= <=>
2022-06-20 00:16:28 +0000

    Convert to fish

--- rad-sale-on-chain/transactions/lock-script-funds.sh
+++ rad-sale-on-chain/transactions/lock-script-funds.sh
@@ -1,21 +1,21 @@
-utxoskey=../octoberFestMetaData/minting.skey
+set utxoskey ../octoberFestMetaData/minting.skey
 
-sellerAddress="addr_test1vrh0kkuahtz28qpfdhsx2hm2eekf06des8h03xnm757u65sd6egwy"
-scriptAddress=`cardano-cli address build --payment-script-file result.plutus --testnet-magic 1097911063`
-scriptTxOut="$scriptAddress+3000000+3 641593ca39c5cbd3eb314533841d53e61ebf6ee7a0ec7c391652f31e.43617264616e6961466f756e6465725768697465"
-sellerAddressUtxo="37607131022445c438e0512e1de44b8e0f27650036a835c3c2a3417ffe36ee54#0"
+set sellerAddress "addr_test1vrh0kkuahtz28qpfdhsx2hm2eekf06des8h03xnm757u65sd6egwy"
+set scriptAddress (eval cardano-cli address build --payment-script-file /home/jonathan/Documents/cardania-contracts/rad-sale-on-chain/transactions/result.plutus --testnet-magic 1097911063)
+set scriptTxOut "$scriptAddress+3000000+1 641593ca39c5cbd3eb314533841d53e61ebf6ee7a0ec7c391652f31e.43617264616e6961466f756e6465725768697465"
+set sellerAddressUtxo "27a3db2bc0b56082b64bd8e0bf5d27b929203964440152cbef62baefc25cb9d7#0"
 
+echo $scriptAddress
 cardano-cli transaction build \
   --alonzo-era \
   --cardano-mode \
   --testnet-magic 1097911063 \
-  --tx-in "433eb9e97aa2a47814c52182e7ed3bce6943bb4fc73bf2157840cfcbb0d02ecc#1" \
-  --tx-in "59b5a23ea877c99cdb54700bf0e1e72f3bd399f552100d36a8a0125317b452a7#0" \
-  --tx-in "$sellerAddressUtxo" \
-  --tx-out "$scriptTxOut" \
-  --tx-out-datum-hash-file unit.json \
-  --change-address "$sellerAddress" \
-  --protocol-params-file ~/Documents/rad-sale-on-chain/transactions/protocol.json \
+  --tx-in "d8f14ad20abddcbcd9d67ef024b425fd072ace37e27367e7a66d8260c4ec0744#0" \
+  --tx-in $sellerAddressUtxo \
+  --tx-out $scriptTxOut \
+  --tx-out-datum-hash-file /home/jonathan/Documents/cardania-contracts/rad-sale-on-chain/transactions/unit.json \
+  --change-address $sellerAddress \
+  --protocol-params-file /home/jonathan/Documents/cardania-contracts/rad-sale-on-chain/transactions/protocol.json \
   --out-file "tx.body"
 
 cardano-cli transaction sign \
