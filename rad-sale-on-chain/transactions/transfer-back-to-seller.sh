6e7028bba333f5ce2b5ff8d95366c32a107abf54
= <=>
2022-06-24 15:34:38 +0000

    Convert to fish

--- rad-sale-on-chain/transactions/transfer-back-to-seller.sh
+++ rad-sale-on-chain/transactions/transfer-back-to-seller.sh
@@ -1,27 +1,28 @@
-utxoskey=../octoberFestMetaData/buyer.skey
+#!/usr/bin/env fish
+set utxoskey /home/jonathan/Documents/octoberFestMetaData/buyer.skey
 
-addresOne="addr_test1vrtmnwrqfnwgnl35y8kg0e8euuqn7yyl5th700k3ezuz30q8hrt37"
-addresTwo="addr_test1vrh0kkuahtz28qpfdhsx2hm2eekf06des8h03xnm757u65sd6egwy"
-addressTwoTxOut="$addresTwo+1500000+1 641593ca39c5cbd3eb314533841d53e61ebf6ee7a0ec7c391652f31e.43617264616e6961466f756e6465725768697465"
-addressOneUtxo="0c200fd6ade8dcf81c2f9088583e2a8fcacd299b096f4bd8052309f87354964d#1"
+set addresOne "addr_test1vrtmnwrqfnwgnl35y8kg0e8euuqn7yyl5th700k3ezuz30q8hrt37"
+set addresTwo "addr_test1vrh0kkuahtz28qpfdhsx2hm2eekf06des8h03xnm757u65sd6egwy"
+set addressTwoTxOut "$addresTwo+1500000+1 641593ca39c5cbd3eb314533841d53e61ebf6ee7a0ec7c391652f31e.43617264616e6961466f756e6465725768697465"
+set addressOneUtxo "5ef48a84c0af78ca9f665d6d812cf85f97949e7390c9ba1a824204a998d43739#1"
 
 cardano-cli transaction build \
   --alonzo-era \
   --cardano-mode \
   --testnet-magic 1097911063 \
-  --tx-in "0c200fd6ade8dcf81c2f9088583e2a8fcacd299b096f4bd8052309f87354964d#0" \
-  --tx-in "$addressOneUtxo" \
-  --tx-out "$addressTwoTxOut" \
-  --change-address "$addresOne" \
-  --protocol-params-file ~/Documents/rad-sale-on-chain/transactions/protocol.json \
-  --out-file "tx.body"
+  --tx-in "7dfa44bba17617c4204d357b36f289d83a53d8c27c0f5c2e783b97f73d126451#0" \
+  --tx-in $addressOneUtxo \
+  --tx-out $addressTwoTxOut \
+  --change-address $addresOne \
+  --protocol-params-file /home/jonathan/Documents/cardania-contracts/rad-sale-on-chain/transactions/protocol.json \
+  --out-file /home/jonathan/Documents/cardania-contracts/rad-sale-on-chain/transactions/tx.body
 
 cardano-cli transaction sign \
-  --tx-body-file "tx.body" \
+  --tx-body-file /home/jonathan/Documents/cardania-contracts/rad-sale-on-chain/transactions/tx.body \
   --testnet-magic 1097911063 \
-  --signing-key-file "$utxoskey" \
-  --out-file "tx.signed"
+  --signing-key-file $utxoskey \
+  --out-file /home/jonathan/Documents/cardania-contracts/rad-sale-on-chain/transactions/tx.signed
 
 cardano-cli transaction submit \
   --testnet-magic 1097911063 \
-  --tx-file "tx.signed"
+  --tx-file /home/jonathan/Documents/cardania-contracts/rad-sale-on-chain/transactions/tx.signed
