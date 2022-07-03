c3a0fe52233b9b3d180a61494190a2cbaa0fe53c
= <=>
2022-07-03 19:02:23 +0000

    Convert to use lucid

--- rad-sale-on-chain/client/index.ts
+++ rad-sale-on-chain/client/index.ts
@@ -1,109 +1,61 @@
+declare var window: any
 import './style.css'
 import {
-    hasWallet, getWallet
-    , getRewardAddress
-    , buy
-    , getCollateral
-} from '@jonathanfishbein1/cardano-wallet-browser-extensions-interface'
-
+    Lucid, Blockfrost, Utils
+} from 'lucid-cardano'
+const supportedWallets = [
+    'nami',
+    'flint',
+    'eternl'
+]
+    , getWalletApi = async namespace => {
+        return await ('typhon' === namespace) ?
+            window.cardano[namespace]
+            :
+            window.cardano[namespace].enable()
+    }
+    , isSupported = type => supportedWallets.includes(type)
+    , hasWallet = type => isSupported(type) && window.cardano[type.toLowerCase()] !== undefined
 const
     connectButton = document.getElementById('connect'),
-    poolId = "dc508ac7975e573adf6ef17873c74d200e0cd71931c139eb76853281",
+    poolId = "pool1m3gg43uhtetn4hmw79u8836dyq8qe4cex8qnn6mks5egza7n6tp",
     bk = "testnetwIyK8IphOti170JCngH0NedP0yK8wBZs",
     connectMessage = "connect wallet",
-    buyMessage = "buy",
-    buyingMessage = "Buying..."
-    , successMessage = "Successfully bought"
-
-
-
-const protocolParameters = await fetch('https://cardano-testnet.blockfrost.io/api/v0/epochs/latest/parameters'
-    , { headers: { project_id: bk } })
-    .then(res => res.json())
-console.log('protocolParameters' + protocolParameters)
-if (hasWallet('nami') == true) {
-    const wallet = await getWallet('nami')
-    wallet.name = 'Nami'
-    console.log(wallet)
-    console.log(await getCollateral(wallet))
-    const rewardAddress = await getRewardAddress(wallet)
-    const account = await fetch(`https://cardano-testnet.blockfrost.io/api/v0/accounts/${rewardAddress}/`
-        , { headers: { project_id: bk } })
-        .then(res => res.json())
-    //const transaction = await buyTo(wallet, poolId, protocolParameters, account)
-    //console.log('Transaction Hash', transaction)
-}
-
-if (hasWallet('nami') == true) {
-    const wallet = await getWallet('nami')
-        , rewardAddress = await getRewardAddress(wallet)
-        , account = await fetch(`https://cardano-testnet.blockfrost.io/api/v0/accounts/${rewardAddress}/`
-            , { headers: { project_id: bk } })
-            .then(res => res.json())
-    wallet.name = 'Nami'
-    console.log('account ', account)
-    // if (account.active)
-    //     connectButton!.innerText = successMessage
-    // else {
+    buyMessage = "Buy",
+    buyingMessage = "Buying...",
+    unbuyingMessage = "Unbuying..."
+    , successMessage = "Successfully buyd to SUMN!"
+    , lucid = await Lucid.new(
+        new Blockfrost('https://cardano-mainnet.blockfrost.io/api/v0', bk), 'Mainnet')
+if (hasWallet('eternl') == true) {
+    const wallet = await getWalletApi('eternl') as any
+    lucid.selectWallet(wallet)
     connectButton!.innerText = connectMessage
     connectButton?.addEventListener('click', async () => {
         connectButton!.innerText = buyMessage
         connectButton?.addEventListener('click', async () => {
             connectButton!.innerText = buyingMessage
+            const lovelaceAmount = BigInt(Number(10000000))
             const transaction =
-                await buy(wallet, protocolParameters
-                    , "addr_test1vrh0kkuahtz28qpfdhsx2hm2eekf06des8h03xnm757u65sd6egwy"
-                    , '10000000', "addr_test1wruumrldke6wh5rjfkyg9f7xztrh7nzvpzum36jqajnextshh0442")
-            // transaction ?
-            //     connectButton!.innerText = successMessage
-            //     :
-            //     console.log('Transaction Hash', transaction)
+                await lucid
+                    .newTx()
+                    .payToAddress('addr_test1vrh0kkuahtz28qpfdhsx2hm2eekf06des8h03xnm757u65sd6egwy', { lovelace: lovelaceAmount })
+                    .addSigner(wallet.address())
+                    .complete()
+            transaction.txComplete
+            console.log(transaction)
+            console.log(transaction.txComplete)
+            const signedTx = await transaction
+                .sign()
+                .complete()
+            console.log(signedTx)
+            const transactionHash = await signedTx
+                .submit()
+            console.log(transactionHash)
+            transactionHash ?
+                connectButton!.innerText = successMessage
+                :
+                console.log('Transaction Hash', transaction)
         })
     })
-
 }
-//}
-// else
-//     if (hasWallet('Flint') == true) {
-//         const wallet = await getWallet('Flint')
-//         console.log(wallet)
-//         const rewardAddress = await getRewardAddress(wallet)
-//         const account = await fetch(`https://cardano-testnet.blockfrost.io/api/v0/accounts/${rewardAddress}/`
-//             , { headers: { project_id: bk } })
-//             .then(res => res.json())
-//         const transaction = await buyTo(wallet, poolId, protocolParameters, account)
-//         console.log('Transaction Hash', transaction)
-//     }
-
-//     else if (hasWallet('Typhon') == true) {
-//         const wallet = await getWallet('Typhon')
-//         console.log(wallet)
-//         const rewardAddress = await getRewardAddress(wallet)
-//         const account = await fetch(`https://cardano-testnet.blockfrost.io/api/v0/accounts/${rewardAddress}/`
-//             , { headers: { project_id: bk } })
-//             .then(res => res.json())
-//         const transaction = await buyTo(wallet, poolId, protocolParameters, account)
-//         console.log('Transaction Hash', transaction)
-//     }
-// console.log(hasWallet('nami'))
-// if (hasWallet('GeroWallet') == true) {
-//     const wallet = await getWallet('GeroWallet')
-//     console.log(wallet)
-//     const rewardAddress = await getRewardAddress(wallet)
-//     const account = await fetch(`https://cardano-testnet.blockfrost.io/api/v0/accounts/${rewardAddress}/`
-//         , { headers: { project_id: bk } })
-//         .then(res => res.json())
-//     const transaction = await buyTo(wallet, poolId, protocolParameters, account)
-//     console.log('Transaction Hash', transaction)
-// }
-// else
-//     if (hasWallet('yoroi') == true) {
-//         const wallet = await getWallet('yoroi')
-//         console.log(wallet)
-//         const rewardAddress = await getRewardAddress(wallet)
-//         const account = await fetch(`https://cardano-testnet.blockfrost.io/api/v0/accounts/${rewardAddress}/`
-//             , { headers: { project_id: bk } })
-//             .then(res => res.json())
-//         const transaction = await buyTo(wallet, poolId, protocolParameters, account)
-//         console.log('Transaction Hash', transaction)
-//     }
