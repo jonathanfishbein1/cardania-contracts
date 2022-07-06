cbdd6b8f96cf0a62c4f84754cc085691f4691ea8
= <=>
2022-07-06 22:53:59 +0000

    Change alonzo to babbage

--- rad-sale-on-chain/transactions/lock-script-funds.sh
+++ rad-sale-on-chain/transactions/lock-script-funds.sh
@@ -3,13 +3,13 @@ set utxoskey /home/jonathan/Documents/octoberFestMetaData/minting.skey
 
 set sellerAddress "addr_test1vrh0kkuahtz28qpfdhsx2hm2eekf06des8h03xnm757u65sd6egwy"
 set scriptAddress (eval cardano-cli address build --payment-script-file /home/jonathan/Documents/cardania-contracts/rad-sale-on-chain/transactions/result.plutus --testnet-magic 1097911063)
-set scriptTxOut "$scriptAddress+3000000+2 641593ca39c5cbd3eb314533841d53e61ebf6ee7a0ec7c391652f31e.43617264616e6961466f756e6465725768697465"
-set sellerAddressCollateralUtxo "639e0d6038aad1a71a4fab3a544c9580c8e7875048a3e54589dd3f6fcc897096#0"
-set sellerAddressTokenUtxo "fe40f26d6f4e72c845b3c63d359e7a8fab73fe9700e9d2c0a510de625ecc8912#1"
+set scriptTxOut "$scriptAddress+3000000+ 1 641593ca39c5cbd3eb314533841d53e61ebf6ee7a0ec7c391652f31e.43617264616e6961466f756e6465725768697465"
+set sellerAddressCollateralUtxo "98177fe16779295c30a33893828760059d0bf08b21f91a42ad3200670ab0bbd2#0"
+set sellerAddressTokenUtxo "dd85c4d38fb422ec4ac67610915670863582d3b8af08ee2159dbd08fbf639a94#0"
 
 echo $scriptAddress
 cardano-cli transaction build \
-  --alonzo-era \
+  --babbage-era \
   --cardano-mode \
   --testnet-magic 1097911063 \
   --tx-in $sellerAddressTokenUtxo \
