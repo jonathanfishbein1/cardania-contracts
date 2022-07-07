afe9094c1fe60276f7a366ab7c66f91ec77f49b8
= <=>
2022-07-07 18:05:02 +0000

    Update to babbage era

--- rad-sale-on-chain/transactions/transfer-back-to-seller.sh
+++ rad-sale-on-chain/transactions/transfer-back-to-seller.sh
@@ -3,15 +3,15 @@ set utxoskey /home/jonathan/Documents/octoberFestMetaData/buyer.skey
 
 set addresOne "addr_test1vrtmnwrqfnwgnl35y8kg0e8euuqn7yyl5th700k3ezuz30q8hrt37"
 set addresTwo "addr_test1vrh0kkuahtz28qpfdhsx2hm2eekf06des8h03xnm757u65sd6egwy"
-set addressTwoTxOut "$addresTwo+1500000+1 641593ca39c5cbd3eb314533841d53e61ebf6ee7a0ec7c391652f31e.43617264616e6961466f756e6465725768697465"
-set addressOneUtxo "5ef48a84c0af78ca9f665d6d812cf85f97949e7390c9ba1a824204a998d43739#1"
+set addressTwoTxOut "$addresTwo+1500000+3 641593ca39c5cbd3eb314533841d53e61ebf6ee7a0ec7c391652f31e.43617264616e6961466f756e6465725768697465"
 
 cardano-cli transaction build \
-  --alonzo-era \
+  --babbage-era \
   --cardano-mode \
   --testnet-magic 1097911063 \
-  --tx-in "7dfa44bba17617c4204d357b36f289d83a53d8c27c0f5c2e783b97f73d126451#0" \
-  --tx-in $addressOneUtxo \
+  --tx-in "7dfa44bba17617c4204d357b36f289d83a53d8c27c0f5c2e783b97f73d126451#1" \
+  --tx-in "9e0938663296e4da86978fb2926885952af4a1bc5da120062e9ccc249fd4d4c8#1" \
+  --tx-in "c3e7f372ec821a654c2472313730a66890e2aa00835a14585a173a30dfc56cec#1" \
   --tx-out $addressTwoTxOut \
   --change-address $addresOne \
   --protocol-params-file /home/jonathan/Documents/cardania-contracts/rad-sale-on-chain/transactions/protocol.json \
